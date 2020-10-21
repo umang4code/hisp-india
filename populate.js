7ad68b82882f6c61e240ae077e25b21153561daa
umang4code <ag.mr.umang@gmail.com>
2020-10-21 20:16:09 +0530

    correcton in JS file

--- populate.js
+++ populate.js
@@ -4,11 +4,12 @@ xmlhttp.onreadystatechange = function() {
     var myArr = JSON.parse(this.responseText);
   }
 };
+xmlhttp.open("GET", "api/organisationUnits.json?level=4&paging=0", true);
+xmlhttp.send();
 var body = document.getElementsById("demo");
-for(obj of myArr)
+for(obj of myArr){
     newdiv = document.createElement('div');   //create a div
     newdiv.id = obj.id;                       //add an id
     newdiv.innerhtml=obj.displayName;                      
     body.appendChild(newdiv);                 //append to the doc.body
-xmlhttp.open("GET", "api/organisationUnits.json?level=4&paging=0", true);
-xmlhttp.send();
+}
