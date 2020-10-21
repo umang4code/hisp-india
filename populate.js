481e4f6e0cd16ebe2cd8bb3a8ebfb0f54b707f59
umang4code <ag.mr.umang@gmail.com>
2020-10-21 21:21:32 +0530

    Basic trial for populating Org Units and Created Zip With manifest.

--- populate.js
+++ populate.js
@@ -1,15 +1,17 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
+    var myobj = JSON.parse(this.responseText);
+    var myArr=myobj.organisationUnits;
   }
 };
-xmlhttp.open("GET", "api/organisationUnits.json?level=4&paging=0", true);
+xmlhttp.open("GET", "https://ln1.hispindia.org/uphmis230/api/organisationUnits.json?level=4&paging=0", true);
 xmlhttp.send();
-var body = document.getElementsById("demo");
+var body = document.getElementById("demo");
+
 for(obj of myArr){
     newdiv = document.createElement('div');   //create a div
     newdiv.id = obj.id;                       //add an id
     newdiv.innerhtml=obj.displayName;                      
-    body.appendChild(newdiv);                 //append to the doc.body
+   body.appendChild(newdiv);                 //append to the doc.body
 }
