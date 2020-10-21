914f01c0bb3dd05994aacc4eb4af9eb43a7e7304
umang4code <ag.mr.umang@gmail.com>
2020-10-22 00:05:42 +0530

    Array with values stored. Need to figure out how to display them.

--- populate.js
+++ populate.js
@@ -1,17 +1,20 @@
 var xmlhttp = new XMLHttpRequest();
+var myobj;
+var myArr;
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var myobj = JSON.parse(this.responseText);
-    var myArr=myobj.organisationUnits;
+    myobj = JSON.parse(this.responseText);
+    myArr=myobj.organisationUnits;
   }
 };
 xmlhttp.open("GET", "https://ln1.hispindia.org/uphmis230/api/organisationUnits.json?level=4&paging=0", true);
 xmlhttp.send();
 var body = document.getElementById("demo");
-
-for(obj of myArr){
-    newdiv = document.createElement('div');   //create a div
-    newdiv.id = obj.id;                       //add an id
-    newdiv.innerhtml=obj.displayName;                      
-   body.appendChild(newdiv);                 //append to the doc.body
+var x;
+var newpara;
+for(x in myArr){
+    newpara = document.createElement('p');   //create a p
+    newpara.id = myArr[x].id;                       //add an id
+    newpara.innerHTML=myArr[x].displayName;                      
+   document.body.appendChild(newpara);                 //append to the doc.body
 }
