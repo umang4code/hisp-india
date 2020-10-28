a46c30ed0acfd0be03520e1230e779096e042d46
umang4code <ag.mr.umang@gmail.com>
2020-10-28 17:17:29 +0530

    Made changes suggested by Samta Ma'am, List populating now.

--- populate.js
+++ populate.js
@@ -6,14 +6,14 @@ xmlhttp.onreadystatechange = function() {
     myobj = JSON.parse(this.responseText);
     myArr=myobj.organisationUnits;
   }
-};
-xmlhttp.open("GET", "https://ln1.hispindia.org/uphmis230/api/organisationUnits.json?level=4&paging=0", true);
-xmlhttp.send();
-var ou=document.getElementById("orgUnits");
-for(x in myArr){
+	var ou=document.getElementById("orgUnits");
+	for(x in myArr){
     var newItem=document.createElement('li');   //create a list element
     newItem.id=myArr[x].id;		//add an id
     newItem.className="ui-sortable-handle";                      
     newItem.textContent=myArr[x].displayName;                      
    	ou.appendChild(newItem);                 //append to the doc.body
-}
+	}
+};
+xmlhttp.open("GET", "https://ln1.hispindia.org/uphmis230/api/organisationUnits.json?level=4&paging=0", true);
+xmlhttp.send();
