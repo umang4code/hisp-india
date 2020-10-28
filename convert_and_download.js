1778a1dbe6ead6a752e6c03fb83d5dac692e0c8d
umang4code <ag.mr.umang@gmail.com>
2020-10-28 22:20:26 +0530

    Testing array creation

--- convert_and_download.js
+++ convert_and_download.js
@@ -1,8 +1,15 @@
 var weekday={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+var finalArr;
 function convert(){
-	var finalArr= new Array[noDistricts];
+	finalArr= new Array[noDistricts+1];
+	finalArr.push{"District Name","District UID","Weekday"};
 	for(int i=0;i<6;i++)
 	{
-		
+		var wl=document.getElementbyId(weekday[i]);
+		var mylist=wl.getElementsbytag('li');
+		for(item in mylist)
+		{
+			finalArr.push({mylist[item].textContent,mylist[item].id},weekday[i]);
+		}
 	}
 }
