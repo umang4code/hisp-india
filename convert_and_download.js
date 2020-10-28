a63d2c9c9d067b87f50d224d032852a5bc678ac3
umang4code <ag.mr.umang@gmail.com>
2020-10-29 00:33:47 +0530

    Array is populating. Need to convert to csv and download

--- convert_and_download.js
+++ convert_and_download.js
@@ -1,15 +1,18 @@
 var weekday=new Array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 var finalArr;
 function convert(){
-	finalArr= new Array[noDistricts+1];
-	finalArr.push{"District Name","District UID","Weekday"};
-	for(int i=0;i<6;i++)
+	finalArr= new Array(noDistricts+1);
+	var arr_index=0;
+	finalArr[arr_index++]=new Array("District Name","District UID","Weekday");
+	var wl;
+	var mylist;
+	for(var i=0;i<6;i++)
 	{
-		var wl=document.getElementbyId(weekday[i]);
-		var mylist=wl.getElementsbytag('li');
-		for(item in mylist)
+		wl=document.getElementById(weekday[i]);
+		mylist=wl.getElementsByTagName('li');
+		for(item of mylist)
 		{
-			finalArr.push(new Array(mylist[item].textContent,mylist[item].id,weekday[i]));
+			finalArr[arr_index++]=new Array(item.textContent,item.id,weekday[i]);
 		}
 	}
 }
